ace751a3b623922f5003b71e8ceb66bbc512cc4b
钟代麒 <zhongdaiqi@gyyx.cn>
2019-09-12 18:21:28 +0800

    alipay

--- src/main/java/jblog/guohai/org/bll/agent/AlipayAgent.java
+++ src/main/java/jblog/guohai/org/bll/agent/AlipayAgent.java
@@ -1,20 +1,24 @@
 package jblog.guohai.org.bll.agent;
 
-import java.security.GeneralSecurityException;
-
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
+import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSON;
 import com.alipay.api.AlipayClient;
+import com.alipay.api.AlipayConstants;
 import com.alipay.api.CertAlipayRequest;
 import com.alipay.api.DefaultAlipayClient;
-import com.alipay.api.internal.util.codec.Base64;
+import com.alipay.api.domain.AlipayFundTransOrderQueryModel;
+import com.alipay.api.domain.AlipayFundTransToaccountTransferModel;
+import com.alipay.api.domain.AlipayTradeQueryModel;
+import com.alipay.api.request.AlipayFundTransOrderQueryRequest;
+import com.alipay.api.request.AlipayFundTransToaccountTransferRequest;
 import com.alipay.api.request.AlipayTradePagePayRequest;
+import com.alipay.api.request.AlipayTradeQueryRequest;
+import com.alipay.api.response.AlipayFundTransOrderQueryResponse;
+import com.alipay.api.response.AlipayTradeQueryResponse;
 
 import jblog.guohai.org.model.AlipayOrderBean;
 
@@ -32,7 +36,7 @@ public class AlipayAgent {
 
 	@Value("${alipay.privateKey}")
 	private String privateKey;
-	
+
 	@Value("${alipay.alipayPublicKey}")
 	private String alipayPublicKey;
 
@@ -41,42 +45,93 @@ public class AlipayAgent {
 
 	@Value("${alipay.appCertPath}")
 	private String appCertPath;
-	
+
 	/**
 	 * 设置支付宝公钥证书路径
 	 */
 	@Value("${alipay.alipayCertPath}")
 	private String alipayCertPath;
-	
+
 	/**
 	 * 设置支付宝根证书路径
 	 */
 	@Value("${alipay.alipayRootCertPath}")
 	private String alipayRootCertPath;
-	
+
 	/**
 	 * 配置同步通知地址
 	 */
 	@Value("${alipay.returnUrl}")
 	private String returnUrl;
-	
+
 	/**
 	 * 配置异步通知地址
 	 */
 	@Value("${alipay.notifyUrl}")
 	private String notifyUrl;
-	
+
 	@Value("${alipay.aesKey}")
 	private String aesKey;
-	
-	
+
 	/**
 	 * 设置字符集
 	 */
-	@Value("${alipay.charset:UTF-8}")
+	@Value("${alipay.charset:utf-8}")
 	private String charset;
 
-	public String buildPayUrl(AlipayOrderBean orderBean) throws  Exception{
+	public String tradePagePay(AlipayOrderBean orderBean) throws Exception {
+		AlipayClient alipayClient = buildAlipayClient();
+		AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
+		request.setReturnUrl(returnUrl);
+		request.setNotifyUrl(notifyUrl);
+		String json = JSON.toJSONString(orderBean);
+		request.setBizContent(json);
+		request.setNeedEncrypt(true);
+		return alipayClient.pageExecute(request).getBody();
+	}
+	
+	public String tradeQuery(String tradeNo) throws Exception{
+		AlipayClient alipayClient = buildAlipayClient();
+		AlipayTradeQueryRequest request = new AlipayTradeQueryRequest();
+		AlipayTradeQueryModel model = new AlipayTradeQueryModel();
+		model.setOutTradeNo(tradeNo);
+		request.setBizModel(model);	
+		request.setNeedEncrypt(true);
+		AlipayTradeQueryResponse response = alipayClient.certificateExecute(request);
+		// 在这里检查订单的状态值  如果是 ACQ.SYSTEM_ERROR  则本次检查失败，不能更新支付单状态
+		return	response.getBody();
+	}
+
+	public String transfer() throws Exception {
+		AlipayClient alipayClient = buildAlipayClient();
+		AlipayFundTransToaccountTransferRequest request= new AlipayFundTransToaccountTransferRequest();
+		AlipayFundTransToaccountTransferModel model = new AlipayFundTransToaccountTransferModel();
+		model.setAmount("0.1");
+		model.setOutBizNo(String.format("T0001%s", new Date().getTime()));
+		model.setPayeeType("ALIPAY_LOGONID");
+		model.setPayeeAccount("zhongdaiqi@gmail.com");
+		model.setPayerShowName("醉么支付转帐接口测试");
+		model.setPayeeRealName("钟代麒");
+		model.setRemark("转帐测试中");
+		request.setBizModel(model);	
+		request.setNeedEncrypt(true);
+		// 如果是success则修改状态为成功，如果是SYSTEM_ERROR则，不能修改状态，要调用query接口查询结果来决定状态
+	    return	alipayClient.certificateExecute(request).getBody();
+	}
+	
+	public String transferQuery(String bizNo) throws Exception{
+		AlipayClient alipayClient = buildAlipayClient();
+		AlipayFundTransOrderQueryRequest request = new AlipayFundTransOrderQueryRequest();
+		AlipayFundTransOrderQueryModel model = new AlipayFundTransOrderQueryModel();
+		model.setOutBizNo(bizNo);
+		request.setBizModel(model);	
+		request.setNeedEncrypt(true);
+		AlipayFundTransOrderQueryResponse response= alipayClient.certificateExecute(request);
+		// 在这里检查订单的状态值
+		return	response.getBody();
+	}
+
+	private AlipayClient buildAlipayClient() throws Exception {
 		// 构造client
 		CertAlipayRequest certAlipayRequest = new CertAlipayRequest();
 		// 设置网关地址
@@ -86,11 +141,11 @@ public class AlipayAgent {
 		// 设置应用私钥
 		certAlipayRequest.setPrivateKey(privateKey);
 		// 设置请求格式，固定值json
-		certAlipayRequest.setFormat("json");
+		certAlipayRequest.setFormat(AlipayConstants.FORMAT_JSON);
 		// 设置字符集
-		certAlipayRequest.setCharset(charset);
+		certAlipayRequest.setCharset(AlipayConstants.CHARSET_UTF8);
 		// 设置签名类型
-		certAlipayRequest.setSignType(signType);
+		certAlipayRequest.setSignType(AlipayConstants.SIGN_TYPE_RSA2);
 		// 设置应用公钥证书路径
 		certAlipayRequest.setCertPath(appCertPath);
 		// 设置支付宝公钥证书路径
@@ -98,47 +153,8 @@ public class AlipayAgent {
 		// 设置支付宝根证书路径
 		certAlipayRequest.setRootCertPath(alipayRootCertPath);
 		// 构造client
-		AlipayClient alipayClient;
-		alipayClient = new DefaultAlipayClient(certAlipayRequest);
-		
-		// 构造API请求
-		
-		AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
-		request.setReturnUrl(returnUrl);
-		request.setNotifyUrl(notifyUrl);
-		String json = JSON.toJSONString(orderBean);
-		request.setBizContent(json);
-		return alipayClient.pageExecute(request).getBody();
+		certAlipayRequest.setEncryptor(aesKey);
+		certAlipayRequest.setEncryptType(AlipayConstants.ENCRYPT_TYPE_AES);
+		return new DefaultAlipayClient(certAlipayRequest);
 	}
-
-	public String encrypt(String bizContent) throws Exception{
-		String fullAlg = "AES/CBC/PKCS5Padding";
-		 
-        Cipher cipher = Cipher.getInstance(fullAlg);
-        IvParameterSpec iv = new IvParameterSpec(initIv(fullAlg));
-        cipher.init(Cipher.ENCRYPT_MODE,
-                new SecretKeySpec(Base64.decodeBase64(aesKey.getBytes()), "AES"),
-                iv);
- 
-        byte[] encryptBytes = cipher.doFinal(bizContent.getBytes(charset));
-        return new String(Base64.encodeBase64(encryptBytes));
-		
-	}
-	
-	 /**
-     * 初始向量的方法, 全部为0. 这里的写法适合于其它算法,针对AES算法的话,IV值一定是128位的(16字节).
-     *
-     * @param fullAlg
-     * @return
-     * @throws GeneralSecurityException
-     */
-    private static byte[] initIv(String fullAlg) throws GeneralSecurityException {
-        Cipher cipher = Cipher.getInstance(fullAlg);
-        int blockSize = cipher.getBlockSize();
-        byte[] iv = new byte[blockSize];
-        for (int i = 0; i < blockSize; ++i) {
-            iv[i] = 0;
-        }
-        return iv;
-    }
 }
--- src/main/java/jblog/guohai/org/web/AlipayController.java
+++ src/main/java/jblog/guohai/org/web/AlipayController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import jblog.guohai.org.bll.agent.AlipayAgent;
 import jblog.guohai.org.model.AlipayOrderBean;
 
-
 @Controller
 @RequestMapping("/order/alipay")
 public class AlipayController {
@@ -21,20 +20,40 @@ public class AlipayController {
 	@RequestMapping(value = "pay")
 	public String pay(Model model) throws Exception {
 		AlipayOrderBean orderBean = new AlipayOrderBean();
-
+		
 		orderBean.setOutTradeNo(String.format("20000%s", new Date().getTime()));
 		orderBean.setTotalAmount("1.00");
-		orderBean.setSubject("醉么商城订单" +String.format("Test%s", new Date().getTime()));
-		orderBean.setBody("醉么商城订单" +String.format("Test%s", new Date().getTime()));
+		orderBean.setSubject("醉么商城订单" + String.format("Zuime%s", new Date().getTime()));
+		orderBean.setBody("醉么商城订单" + String.format("Zuime%s", new Date().getTime()));
 		orderBean.setProductCode("FAST_INSTANT_TRADE_PAY");
 
-        model.addAttribute("page",alipayAgent.buildPayUrl(orderBean));
-        return "pay/alipay/create";
+		model.addAttribute("page", alipayAgent.tradePagePay(orderBean));
+		return "pay/alipay/create";
+	}
+
+	@RequestMapping(value = "return")
+	public String payReturn() {
+
+		return "pay/alipay/return";
 	}
 	
-	@RequestMapping(value = "return")
-	public String payReturn(){
-		
-		return "pay/alipay/return";
+	@RequestMapping(value = "pay/query")
+	public String payQuery(String tradeNo,Model model) throws Exception {
+		model.addAttribute("page", alipayAgent.tradeQuery(tradeNo));
+		return "pay/alipay/create";
+	}
+
+	@RequestMapping(value = "transfer")
+	public String transfer(Model model) throws Exception {
+		model.addAttribute("page", alipayAgent.transfer());
+		return "pay/alipay/create";
+
+	}
+	
+	@RequestMapping(value = "transfer/query")
+	public String transferQuery(String bizNo,Model model) throws Exception {
+		model.addAttribute("page", alipayAgent.transferQuery(bizNo));
+		return "pay/alipay/create";
+
 	}
 }
