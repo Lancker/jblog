df746bd69b514038d79f3050c4a2553b92cea4ca
钟代麒 <zhongdaiqi@gyyx.cn>
2019-10-12 11:00:13 +0800

    调整配置

---
+++ src/main/java/jblog/guohai/org/bll/agent/TencentCaptchaAgent.java
@@ -0,0 +1,64 @@
+package jblog.guohai.org.bll.agent;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.tencentcloudapi.captcha.v20190722.CaptchaClient;
+import com.tencentcloudapi.captcha.v20190722.models.DescribeCaptchaResultRequest;
+import com.tencentcloudapi.captcha.v20190722.models.DescribeCaptchaResultResponse;
+import com.tencentcloudapi.common.Credential;
+import com.tencentcloudapi.common.exception.TencentCloudSDKException;
+import com.tencentcloudapi.common.profile.ClientProfile;
+
+public class TencentCaptchaAgent {
+
+	/**
+	 * 网关地址
+	 */
+	@Value("${tencent.secretId}")
+	private String secretId;
+
+	@Value("${tencent.secretKey}")
+	private String secretKey;
+	
+	/**
+	 * 网关地址
+	 */
+	@Value("${tencent.captcha.captchaAppId}")
+	private Long captchaAppId;
+
+	@Value("${tencent.captcha.appSecretKey}")
+	private String appSecretKey;
+	
+	
+	public String verify(String ticket,String randstr){
+		 try{
+	            // 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey
+	            Credential cred = new Credential(secretId, secretKey);
+	            
+	            // 实例化要请求产品(以cvm为例)的client对象
+	            ClientProfile clientProfile = new ClientProfile();
+	            clientProfile.setSignMethod(ClientProfile.SIGN_TC3_256);
+
+	            CaptchaClient client = new CaptchaClient(cred, "ap-beijing", clientProfile);
+	            
+	            // 实例化一个请求对象
+	            DescribeCaptchaResultRequest req = new DescribeCaptchaResultRequest();
+	            req.setCaptchaType(9L);
+	            req.setTicket(ticket);
+	            req.setCaptchaAppId(2046804662L);
+	            req.setAppSecretKey("05lhzYMzdWuhUpg3Lfz5Kow**");
+	            req.setRandstr(randstr);
+	            
+	            
+	            // 通过client对象调用想要访问的接口，需要传入请求对象
+	            DescribeCaptchaResultResponse resp = client.DescribeCaptchaResult(req);
+	            
+	            // 输出json格式的字符串回包
+	            System.out.println(DescribeCaptchaResultRequest.toJsonString(resp));
+	            return DescribeCaptchaResultRequest.toJsonString(resp);
+	        } catch (TencentCloudSDKException e) {
+	                System.out.println(e.toString());
+	            	return e.toString();
+	        }
+	}
+}
--- src/main/java/jblog/guohai/org/web/TencentController.java
+++ src/main/java/jblog/guohai/org/web/TencentController.java
@@ -32,35 +32,7 @@ public class TencentController {
 	@RequestMapping(value = "verify")
 	@ResponseBody
 	public String verify(Model model,String ticket,String randstr) throws Exception {
-	     try{
-	            // 实例化一个认证对象，入参需要传入腾讯云账户secretId，secretKey
-	            Credential cred = new Credential("2046804662", "05lhzYMzdWuhUpg3Lfz5Kow**");
-	            
-	            // 实例化要请求产品(以cvm为例)的client对象
-	            ClientProfile clientProfile = new ClientProfile();
-	            clientProfile.setSignMethod(ClientProfile.SIGN_TC3_256);
-
-	            CaptchaClient client = new CaptchaClient(cred, "ap-beijing", clientProfile);
-	            
-	            // 实例化一个请求对象
-	            DescribeCaptchaResultRequest req = new DescribeCaptchaResultRequest();
-	            req.setCaptchaType(9L);
-	            req.setTicket(ticket);
-	            req.setCaptchaAppId(2046804662L);
-	            req.setAppSecretKey("05lhzYMzdWuhUpg3Lfz5Kow**");
-	            req.setRandstr(randstr);
-	            
-	            
-	            // 通过client对象调用想要访问的接口，需要传入请求对象
-	            DescribeCaptchaResultResponse resp = client.DescribeCaptchaResult(req);
-	            
-	            // 输出json格式的字符串回包
-	            System.out.println(DescribeCaptchaResultRequest.toJsonString(resp));
-	            return DescribeCaptchaResultRequest.toJsonString(resp);
-	        } catch (TencentCloudSDKException e) {
-	                System.out.println(e.toString());
-	            	return e.toString();
-	        }
+	    
 	
 	}
 }
