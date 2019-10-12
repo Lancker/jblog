0205399b45b365efa41ceb56b3652de625139458
钟代麒 <zhongdaiqi@gyyx.cn>
2019-10-12 11:05:16 +0800

    重构

--- src/main/java/jblog/guohai/org/web/TencentController.java
+++ src/main/java/jblog/guohai/org/web/TencentController.java
@@ -1,38 +1,36 @@
 
 package jblog.guohai.org.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.tencentcloudapi.captcha.v20190722.CaptchaClient;
-import com.tencentcloudapi.captcha.v20190722.models.DescribeCaptchaResultRequest;
-import com.tencentcloudapi.captcha.v20190722.models.DescribeCaptchaResultResponse;
-import com.tencentcloudapi.common.Credential;
-import com.tencentcloudapi.common.exception.TencentCloudSDKException;
-import com.tencentcloudapi.common.profile.ClientProfile;
-import com.tencentcloudapi.cvm.v20170312.CvmClient;
-import com.tencentcloudapi.cvm.v20170312.models.DescribeZonesRequest;
+import jblog.guohai.org.bll.agent.TencentCaptchaAgent;
 
 /**
  * 腾讯云
+ * 
  * @author zhongdaiqi
  *
  */
 @Controller
 @RequestMapping("/tencent")
 public class TencentController {
+
+	@Autowired
+	private TencentCaptchaAgent tencentCaptchaAgent;
+
 	@RequestMapping(value = "captcha")
 	public String captcha(Model model) throws Exception {
 
 		return "tencent/captcha";
 	}
-	
+
 	@RequestMapping(value = "verify")
 	@ResponseBody
-	public String verify(Model model,String ticket,String randstr) throws Exception {
-	    
-	
+	public String verify(Model model, String ticket, String randstr) throws Exception {
+		return tencentCaptchaAgent.verify(ticket, randstr);
 	}
 }
