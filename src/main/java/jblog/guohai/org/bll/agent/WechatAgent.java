49d63fec9a802f3c0c7b7f243b3271463f04a8bb
钟代麒 <zhongdaiqi@gyyx.cn>
2019-11-28 14:25:06 +0800

    修正

--- src/main/java/jblog/guohai/org/bll/agent/WechatAgent.java
+++ src/main/java/jblog/guohai/org/bll/agent/WechatAgent.java
@@ -9,7 +9,7 @@ import jblog.guohai.org.util.HttpUtil;
 @Service
 public class WechatAgent {
 
-	private static String ACCESS_TOKEN_URL_TPL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code";
+	private static String ACCESS_TOKEN_URL_TPL = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code";
 	@Value("${wechat.appId}")
 	private static String appId;
 	@Value("${wechat.secret}")
